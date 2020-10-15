a1c1a7ae9c6596c5ddf5816fd3ce25ef63a2895f
bijiajin <bi_jiajin@163.com>
2020-10-15 17:59:14 +0800

    学习示例文件

---
+++ vue_02/src/components/HelloVue.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="classA">
+		<h2> {{msg}}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name:"HelloVue",
+	props:{
+		msg:String
+	}
+}
+</script>
+
+<style>
+.classA{
+	color: orange;
+}
+</style>
